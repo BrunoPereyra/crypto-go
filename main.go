e2ac7adcc7ac69653eebedd6659e7960675612de
Bruno Pereyra <brunopereyra459@gmail.com>
2023-06-08 14:41:10 -0300

    crypto1

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	"fmt"
 )
 
 func main() {
-	// plainText := "Hola este es lo que tiene que ser secreto"
+	plaintext := []byte("Este es un mensaje secretooo")
+
 	key := make([]byte, 32)
 	if _, err := rand.Read(key); err != nil {
 		fmt.Println("Error al generar la clave:", err)
@@ -20,4 +22,23 @@ func main() {
 		return
 	}
 
+	iv := make([]byte, aes.BlockSize)
+	if _, err := rand.Read(iv); err != nil {
+		fmt.Println("Error al generar el vector de inicialización:", err)
+		return
+	}
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+
+	ciphertext := make([]byte, len(plaintext))
+	mode.CryptBlocks(ciphertext, plaintext)
+
+	fmt.Println("Texto cifrado:", ciphertext)
+	fmt.Println("Clave:", key)
+
+	mode = cipher.NewCBCDecrypter(block, iv)
+	decrypted := make([]byte, len(ciphertext))
+	mode.CryptBlocks(decrypted, ciphertext)
+
+	fmt.Println("Texto decifrado:", string(decrypted))
 }
